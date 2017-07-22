b3f26bf50c9d627b79c1c256f9bd7d12de524f0b
Daniel Castro <castrodd@gmail.com>
2017-07-22 14:45:16 -0400

    Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, length: { minimum: 8, too_short: "%{count} characters is the minimum required." }
   has_many :documents
 end
