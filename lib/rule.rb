b7cd338979609bce9dd93b1018b56ca06762d88b
Daniel Castro <castrodd@gmail.com>
2017-07-23 13:38:12 -0400

    Methods.

--- lib/rule.rb
+++ lib/rule.rb
@@ -14,11 +14,21 @@ ahead," or "O.K." Properly written as two words — all right.'
     end
   end
 
+  def allude(str, obj)
+    if str.include?('allude')
+      obj['allude'] = 'Do not confuse with elude. You allude to a book; you elude a pursuer. Note, too,
+that allude is not synonymous with refer. An allusion is an indirect mention, a reference is
+a specific one.'
+    end
+  end
+  
   def allusion(str, obj)
     if str.include?('allusion')
       obj['allusion'] = 'Easily confused with illusion. The first means "an indirect reference"; the second
       means "an unreal image" or "a false impression".'
     end
   end
+
+
 
 end
