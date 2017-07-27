5642952bc4a6f2468e62d7f5c5044046ac7ac174
Daniel Castro <castrodd@gmail.com>
2017-07-26 21:32:54 -0400

    jQuery added

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,3 +1,11 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on('turbolinks:load', function() {
+  $('mark').click(function() {
+    var id = $(this).attr('id');
+    $('p').css("background-color", "white");
+    $('p#'+id).css("background-color", "yellow");
+
+  })
+})
